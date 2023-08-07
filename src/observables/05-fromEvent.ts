6ac323040ec1626d5b0ef62d78ecef15edd797b6
Eimer Castro <ecastro@corelogic.com>
2023-08-07 11:44:39 -0500

    Implementing fromEvent method to create observables based on DOM events.

---
+++ src/observables/05-fromEvent.ts
@@ -0,0 +1,24 @@
+import { fromEvent } from "rxjs";
+
+/**
+ * DOM events
+ */
+const source1$ = fromEvent<MouseEvent>(document, 'click');
+const source2$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+const observer = {
+    next: (value) => console.log('next', value)
+};
+
+//source1$.subscribe(observer);
+/* source1$.subscribe(event => {
+    console.log(event.x);
+    console.log(event.y);
+}); */
+source1$.subscribe(({x, y}) => {
+    console.log(x, y);
+});
+
+source2$.subscribe(event => {
+    console.log(event.key);
+});
